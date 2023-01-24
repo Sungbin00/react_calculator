6bed3ef6e6e3cdd44991739b47ecd5c467bd06bd
Sungbin00 <ddusin0614@naver.com>
2023-01-25 02:05:21 +0900

    add javascript

--- src/App.js
+++ src/App.js
@@ -9,32 +9,87 @@ function App() {
   const [total, setTotal] = useState(false);
 
   const inputNum = e => {
+    if (curState.includes(".") && e.target.innerText === ".") return
 
+    if (total) {
+      setPreState("")
+    }
+
+    curState
+      ? setCurState((pre) => pre + e.target.innerText)
+      : setCurState(e.target.innerText);
+    setTotal(false);
   };
 
-  const operatorType = e => {
+  useEffect(()=> {
+    setInput(curState);
+  }, [curState])
 
+  useEffect(()=> {
+    setInput("0");
+  }, [])
+
+  const operatorType = e => {
+    setTotal(false);
+    setOperator(e.target.innerText)
+    if (curState === "") return
+    if (preState !== "") {
+      equals();
+    } else {
+      setPreState(curState);
+      setCurState("");
+    }
   };
 
   const equals = e => {
-
+    if (e?.target.innerText === "=") {
+      setTotal(true)
+    }
+    let cal;
+    switch (operator) {
+      case "/" :
+        cal = String(parseFloat(preState) / parseFloat(curState));
+        break;
+      case "+" :
+        cal = String(parseFloat(preState) + parseFloat(curState));
+        break;
+      case "X" :
+        cal = String(parseFloat(preState) * parseFloat(curState));
+        break;
+      case "-" :
+        cal = String(parseFloat(preState) - parseFloat(curState));
+        break;
+      default :
+        return
+    }
+    setInput("");
+    setPreState(cal);
+    setCurState("");
   };
 
   const minusPlus = () => {
-
+    if (curState.charAt(0) === "-") {
+      setCurState(curState.substring(1));
+    } else {
+      setCurState("-"+curState);
+    }
   };
 
   const percent = () => {
-
+    preState ? setCurState(String(parseFloat(curState) / 100 * preState)) : setCurState(String(parseFloat(curState) / 100))
   };
 
   const reset = () => {
-
+    setPreState("");
+    setCurState("");
+    setInput("0");
   };
   return (
     <div className='container'>
       <div className='wrapper'>
-        <div className='screen'>{input}</div>
+        <div className='screen'>
+          {input !== "" || input === "0" ? input : preState}
+        </div>
         <div className='btn light-gray' onClick={reset}>AC</div>
         <div className='btn light-gray' onClick={percent}>%</div>
         <div className='btn light-gray' onClick={minusPlus}>+/-</div>
